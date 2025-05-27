3d7c2e87abd6f919a1acc4ec0286c17a12bb3ca1
lalitgawas <lgxd0708@gmail.com>
2025-05-27 12:54:21 +0530

    added

---
+++ recursion/nextpermute.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+// optimised O(n*logn)
+    void nextPermutation(vector<int>& nums) {
+    int ind=-1;
+    int n=nums.size();
+
+    for(int i=n-2;i>=0;i--){
+        if(nums[i+1]>nums[i]){
+            ind=i;
+            break;
+        }
+    }
+
+    if(ind==-1){
+        sort(nums.begin(),nums.end());
+        return;
+    }
+
+    for(int i=n-1;i>ind;i--){
+        if(nums[i]>nums[ind]){
+            swap(nums[i],nums[ind]);
+            break;
+        }
+    }
+
+    sort(nums.begin()+(ind+1),nums.end());
+    }
+};
